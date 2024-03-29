1a8c15ee9cafa4b45153cc62ee7ce6c579c193f8
Alex <devtools@userway.org>
2024-03-29 09:08:02 +0200

    update userway config

--- userway.config.ts
+++ userway.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from '@userway/a11y-kit';
 
 export default {
-  token: '2bpsb73rkunqxzfvbf0g1cogu0rpl2o4moih3ulc',
-  organization: 'alex-userway-org',
-  project: 'sample-cypress-project-github',
+  token: '7ax2qg0qce9bm0frq5bwo1auqovs3tq0c6l0xleh',
+  organization: 'alexander-barabash-userway-org-tfdls',
+  project: 'test',
   timeout: 100_000_000,
 } satisfies Config;
